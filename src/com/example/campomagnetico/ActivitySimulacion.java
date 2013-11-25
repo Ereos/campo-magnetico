149450179b8382f2646bd033518c1ab1b004d1e8
DaniSum41 <dani13sum41@gmail.com>
2013-11-25 10:37:22 +0100

    Por fin subo lo de las putas tabs :')

--- src/com/example/campomagnetico/ActivitySimulacion.java
+++ src/com/example/campomagnetico/ActivitySimulacion.java
@@ -3,8 +3,10 @@ package com.example.campomagnetico;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TabHost;
 import android.widget.TextView;
 
 public class ActivitySimulacion extends Activity {
@@ -15,6 +17,30 @@ public class ActivitySimulacion extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_simulacion);
+		
+		//Tabs
+		Resources res = getResources();
+		 
+		TabHost tabs=(TabHost)findViewById(android.R.id.tabhost);
+		tabs.setup();
+		 
+		TabHost.TabSpec spec=tabs.newTabSpec("tab1");
+		spec.setContent(R.id.tabConductorA);
+		spec.setIndicator("Conductor A", res.getDrawable(android.R.drawable.ic_dialog_map));
+		tabs.addTab(spec);
+		 
+		spec=tabs.newTabSpec("tab2");
+		spec.setContent(R.id.tabConductorB);
+		spec.setIndicator("Conductor B", res.getDrawable(android.R.drawable.ic_dialog_map));
+		tabs.addTab(spec);
+		
+		spec=tabs.newTabSpec("tab3");
+		spec.setContent(R.id.tabConductorC);
+		spec.setIndicator("Conductor C", res.getDrawable(android.R.drawable.ic_dialog_map));
+		tabs.addTab(spec);
+		 
+		tabs.setCurrentTab(0);
+		//Fin Tabs
 	}
 
 	@Override
