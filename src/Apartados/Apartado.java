e7776afc27bcb93fe453a2a2470b045df787bc48
sermodi <sergiomodino@hotmail.com>
2013-11-26 20:18:28 +0100

    Primera edición de la simulación

--- src/Apartados/Apartado.java
+++ src/Apartados/Apartado.java
@@ -1,4 +1,9 @@
 package Apartados;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+
 /**
  * Genera y administra cada uno de los Arrays de los apartados de la simulación práctica.
  *
@@ -7,6 +12,7 @@ public class Apartado {
 
 	protected double apartado[][];
 	protected int tipo;
+    final protected static MathContext escala = new MathContext(5);
 	
 	/**
 	 * El numero de la opción indica que apartado va a ser creado, inicializa el
@@ -35,13 +41,16 @@ public class Apartado {
 		
 		int indice;
 		if(tipo==1){
-			indice = (int) (y/5);
+			indice = (int) (y/5) - 1;
 		}else{
 			//y = -0.04 + 0.00025 * i
 			indice= (int) ((y + 0.04)/0.0025);
 		}
+		BigDecimal result = new BigDecimal(apartado[indice][1]).round(escala);
+		result = result.multiply(new BigDecimal(1000)).round(escala); //Pasar a miliTeslas
 		
-		return apartado[indice][1];
+		
+		return result.setScale(4, RoundingMode.HALF_UP).doubleValue();
 		
 	}
 
--- src/com/example/campomagnetico/ActivitySimulacion.java
+++ src/com/example/campomagnetico/ActivitySimulacion.java
@@ -1,17 +1,36 @@
 package com.example.campomagnetico;
 
+import java.math.MathContext;
+
+import Apartados.Apartado;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TabHost;
 import android.widget.TextView;
 
 public class ActivitySimulacion extends Activity {
 	
 	protected Intent intent;
+	/** Un seekBar por apartado*/
+	protected SeekBar seekbarA;
+	protected SeekBar seekbarB;
+	protected SeekBar seekbarC;
+
+	/** Un Textview por seekbar */
+	protected TextView seektextA;
+	protected TextView seektextB;
+	protected TextView seektextC;
+	
+	/** Un Textview por apartado*/
+	protected TextView tesla1;
+	protected TextView tesla2;
+	protected TextView tesla3;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +60,56 @@ public class ActivitySimulacion extends Activity {
 		 
 		tabs.setCurrentTab(0);
 		//Fin Tabs
+		
+		//Inicializacion de teslamometros
+		final Apartado apartado1 = new Apartado(1);
+		final Apartado apartado2 = new Apartado(2);
+		final Apartado apartado3 = new Apartado(3);
+		
+		//Fin Teslamometros
+		
+		//Seekbars y sus textview asociados
+
+		seekbarA = (SeekBar) findViewById(R.id.seekBarA);
+		seektextA = (TextView) findViewById(R.id.textSeekA);
+		tesla1 = (TextView) findViewById(R.id.Tesla1);
+		seekbarA.setOnSeekBarChangeListener(new OnSeekBarChangeListener(){
+
+			@Override
+			/**
+			  * Evento encargado del control del estado del seekbar
+			  */
+			 public void onProgressChanged(SeekBar seekBar, int progress,
+			    		boolean fromUser) {
+					
+			    	// TODO Añadir una variable global que tenga el contenido?
+					seektextA.setText("" + (progress*5) + " mA");
+					if(progress == 0){
+						tesla1.setText("0 (T)");
+					}else{
+						tesla1.setText(""+ apartado1.getB(progress*5) + " (mT)");
+					}
+			    }
+
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {
+				// Nada que hacer
+				
+			}
+
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {
+				// Nada que hacer
+				
+			}
+			
+		});
+		/* Seek aún no creados TODO
+		seekbarB = (SeekBar) findViewById(R.id.seekBarB);
+		seektextB = (TextView) findViewById(R.id.textSeekB);
+		seekbarC = (SeekBar) findViewById(R.id.seekBarC);
+		seektextC = (TextView) findViewById(R.id.textSeekC);*/
+		//Fin SeekViews
 	}
 
 	@Override
@@ -64,5 +133,7 @@ public class ActivitySimulacion extends Activity {
 	            return super.onOptionsItemSelected(item);
 	    }
 	}
+	 
+	 
 
 }
