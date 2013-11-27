3bcc8c46766dddea4c16c9ac0b0d5391230eb935
sermodi <sergiomodino@hotmail.com>
2013-11-27 11:00:46 +0100

    Datos v0.5

---
+++ src/Apartados/Datos.java
@@ -0,0 +1,24 @@
+package Apartados;
+
+import java.util.ArrayList;
+
+public class Datos {
+
+	protected int apartado;
+	protected ArrayList<Medida> arrayDatos;
+	
+	
+	public Datos(int apartado) {
+		// TODO Auto-generated constructor stub
+		this.apartado = apartado;
+	}
+	
+	public void add_dato(double y, double campo){
+		
+	}
+	
+	public void get_array(int indice){
+		
+	}
+
+}
---
+++ src/Apartados/Medida.java
@@ -0,0 +1,63 @@
+package Apartados;
+
+public class Medida {
+
+	double valor1;
+	double valor2;
+	double valorIc;
+	int apartado;
+	
+	/**
+	 * Creador de una medida realizada para los apartados 2 y 3.
+	 * @param valor1 Distancia de medida.
+	 * @param valor2 Campo magnético resultante en el punto.
+	 */
+	public Medida(double valor1, double valor2) {
+		super();
+		this.valor1 = valor1;
+		this.valor2 = valor2;
+		this.apartado = 2;
+	}
+
+	public Medida(double valor1, double valor2, double valorIc) {
+		super();
+		this.valor1 = valor1;
+		this.valor2 = valor2;
+		this.valorIc = valorIc;
+		this.apartado = 1;
+	}
+	
+	public double[] get(){
+		if(apartado <= 1){
+			double result[] = new double[2];
+			result[0] = valor1;
+			result[1] = valor2;
+			return result;
+		}else{
+			double result[] = new double[3];
+			result[0] = valor1;
+			result[1] = valor2;
+			result[2] = valorIc;
+			return result;
+		}
+	}
+
+	public double getValor1() {
+		return valor1;
+	}
+
+	public double getValor2() {
+		return valor2;
+	}
+	/**
+	 * Devuelve el valor de la corriente en Amperios
+	 * @requiere Si el apartado es 1, devuelve valor, sino devuelve null.
+	 */
+	public Double getValorIc() {
+		if(this.apartado ==1){
+			return valorIc;
+		}
+		return null;
+	}
+
+}
